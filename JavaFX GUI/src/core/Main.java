ad94f91dbc7b7dafb12e49adcbb5a534f4b4fad5
SaltCube <SaltCube@protonmail.com>
2018-02-11 15:15:29 -0500

    Button Changes

---
+++ JavaFX GUI/src/core/Main.java
@@ -0,0 +1,26 @@
+package core;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+		primaryStage.setTitle("Ordering System");
+		primaryStage.setScene(new Scene(root, 1280, 720));
+		primaryStage.show();
+	}
+
+
+	public static void main(String[] args)
+	{
+        launch(args);
+    }
+}
---
+++ JavaFX GUI/src/core/MainController.java
@@ -0,0 +1,25 @@
+package core;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController
+{
+	@FXML Button Delivery;
+	@FXML Button Pickup;
+	@FXML Button EatIn;
+	@FXML Button ToGo;
+
+	@FXML Button Record;
+
+	@FXML Button Report;
+	@FXML Button Info;
+	@FXML Button Settings;
+
+	@FXML public void Delivery()
+	{
+		//create a new Window
+
+	}
+
+}
