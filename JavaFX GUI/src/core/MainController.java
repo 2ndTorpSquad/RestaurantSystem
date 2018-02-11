c0e6993fa098e41c5aefc35faa4d07a58fba8049
SaltCube <SaltCube@protonmail.com>
2018-02-11 17:19:42 -0500

    GUI edits
    Added new window, updated new GUI data. Replaced old files

--- JavaFX GUI/src/core/MainController.java
+++ JavaFX GUI/src/core/MainController.java
@@ -1,7 +1,11 @@
 package core;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainController
 {
@@ -16,9 +20,78 @@ public class MainController
 	@FXML Button Info;
 	@FXML Button Settings;
 
-	@FXML public void Delivery()
+	byte deliveryType = 0;
+	byte toGoType = 1;
+	byte pickupType = 2;
+	byte eatInType = 3;
+	@FXML private void Order(byte orderType)
 	{
-		//create a new Window
+//		switch (orderType)
+//		{
+//			case (0):
+//			{
+//
+//				break;
+//			}
+//
+//			case (1):
+//			{
+//
+//				break;
+//			}
+//
+//			case (2):
+//			{
+//
+//				break;
+//			}
+//
+//			case (3):
+//			{
+//
+//				break;
+//			}
+//
+//			default:
+//			{
+//
+//				break;
+//			}
+//		}
+		try
+		{
+			Stage settings = new Stage(); //opens a new stage for the OrderWindow
+			settings.setTitle("Order"); //sets title
+			//settings.getIcons().add(new Image("core/icon.png")); //sets icon
+			settings.setScene(new Scene(FXMLLoader.load(getClass().getResource("OrderWindow.fxml")))); //sets GUI file
+			settings.setAlwaysOnTop(true); //sets OrderWindow to always show on top of desktop (until closed)
+			settings.show(); //shows the OrderWindow
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			//new Tracer(e).showAlert();
+		}
+
+	}
+	@FXML private void Delivery()
+	{
+		//call a new Window
+		Order(deliveryType);
+	}
+	@FXML private void ToGo()
+	{
+		Order(toGoType);
+
+	}
+	@FXML private void Pickup()
+	{
+		Order(pickupType);
+
+	}
+	@FXML private void EatIn()
+	{
+		Order(eatInType);
 
 	}
 
